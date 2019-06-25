5d29ac26bd105c3e5018cf0b5de2abc86be6284c
nigeljohnwade <nigeljohnwade@gmail.com>
2019-06-25 23:11:33 +0100

    Refactoring

--- clashAPI.js
+++ clashAPI.js
@@ -1,5 +1,9 @@
-const playerData = (tag) => {
-    return fetch(`clash_data/clash_profile_data_${tag}_06-25-2019-21-41-12.json`)
+const getJSON = url => {
+    return fetch(url)
         .then(response => response.json())
         .then(data => data);
+};
+
+const getPlayer = name =>{
+    return getJSON(`clash_data/clash_profile_data_${name}_06-25-2019-21-41-12.json`)
 };
